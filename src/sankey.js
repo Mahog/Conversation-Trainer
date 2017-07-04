c4b5218c44dcc8cd00cbe7748a6135d5d882249b
mahog <s0020279@mail.zih.tu-dresden.de>
2017-07-04 17:06:03 +0200

    fix: height of nodes

--- src/sankey.js
+++ src/sankey.js
@@ -118,7 +118,7 @@ d3.sankey = function() {
   // Compute the value (size) of each node by summing the associated links.
   function computeNodeValues() {
     nodes.forEach(function(node) {
-      node.value = 0.1;
+      node.value = 0.5;
       node.value += conversations.filter(function(conversation) {
         return conversation.indexOf(node) > -1;
       }).length;
@@ -130,24 +130,24 @@ d3.sankey = function() {
 
     // Iteratively control and fix the value of each node by summing up the
     // associated links and splitting the maximum among the smaller of the two
-    // for (let i = 0; i < 10; i++) {
-    //   nodes.forEach(function(node) {
-    //     let sourceValueSum = d3.sum(node.sourceLinks, value);
-    //     let targetValueSum = d3.sum(node.targetLinks, value);
-    //
-    //     node.value = Math.max(sourceValueSum, targetValueSum);
-    //
-    //     if (sourceValueSum > targetValueSum) {
-    //       node.targetLinks.forEach(function(link) {
-    //         link.value = sourceValueSum / node.targetLinks.length;
-    //       })
-    //     } else if (targetValueSum > sourceValueSum) {
-    //       node.sourceLinks.forEach(function(link) {
-    //         link.value = targetValueSum / node.sourceLinks.length;
-    //       })
-    //     }
-    //   });
-    // }
+    for (let i = 0; i < 10; i++) {
+      nodes.forEach(function(node) {
+        let sourceValueSum = d3.sum(node.sourceLinks, value);
+        let targetValueSum = d3.sum(node.targetLinks, value);
+
+        node.value = Math.max(sourceValueSum, targetValueSum);
+
+        if (sourceValueSum > targetValueSum) {
+          node.targetLinks.forEach(function(link) {
+            link.value = sourceValueSum / node.targetLinks.length;
+          })
+        } else if (targetValueSum > sourceValueSum) {
+          node.sourceLinks.forEach(function(link) {
+            link.value = targetValueSum / node.sourceLinks.length;
+          })
+        }
+      });
+    }
   }
 
   // Iteratively assign the breadth (x-position) for each node.
